c74bf9f4676e0c73b38a867cfa6d8b4e9112e4d3
BartekGawel99 <bartekgawel99@gmail.com>
2022-06-20 20:45:32 +0200

    Add project files.

---
+++ INZFoodie/View/BarcodeScannerView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace INZFoodie.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BarcodeScannerView : ContentPage
+    {
+        public BarcodeScannerView()
+        {
+            InitializeComponent();
+        }
+        private void scanView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned Result", result.Text, "OK");
+            });
+        }
+
+    }
+}
