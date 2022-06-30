970fc399e386d372c9c51c7eeaee2d704b4145b8
BartekGawel99 <bartekgawel99@gmail.com>
2022-06-30 16:43:30 +0200

    Add models Product & Ingredient

---
+++ INZFoodie/Model/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INZFoodie.Model
+{
+    public class Ingredient
+    {
+        [JsonProperty("idIgredient")]
+        public string IdIgredient { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("info")]
+        public string Info { get; set; }
+        [JsonProperty("healthInfo")]
+        public int HealthInfo { get; set; }       
+  
+    }
+}
---
+++ INZFoodie/Model/Product.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace INZFoodie.Model
+{
+    public class Product
+    {
+        [JsonProperty("Barcode")]
+        public string Barcode { get; set; } = string.Empty;
+        [JsonProperty("ProductName")]
+        public string ProductName { get; set; } = string.Empty;
+        [JsonProperty("Producer")]
+        public string Producer { get; set; } = string.Empty;
+        [JsonProperty("ProductWeight")]
+        public string ProductWeight { get; set; } = string.Empty;
+        [JsonProperty("Kcal100")]
+        public string Kcal100 { get; set; } = string.Empty;
+        [JsonProperty("KcalTotal")]
+        public string KcalTotal { get; set; } = string.Empty;
+        [JsonProperty("Fat")]
+        public string Fat { get; set; } = string.Empty;
+        [JsonProperty("SaturatesFat")]
+        public string SaturatesFat { get; set; } = string.Empty;
+        [JsonProperty("Carbohydrate")]
+        public string Carbohydrate { get; set; } = string.Empty;
+        [JsonProperty("Sugar")]
+        public string Sugar { get; set; } = string.Empty;
+        [JsonProperty("Protein")]
+        public string Protein { get; set; } = string.Empty;
+        [JsonProperty("Salt")]
+        public string Salt { get; set; } = string.Empty;
+        [JsonProperty("ingredients")]
+        public List<Ingredient> Ingredients { get; set; } 
+    }
+}
